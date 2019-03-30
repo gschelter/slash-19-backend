c414823febaada1c527a00c3a33ecdf712926b09
Gerrit Schelter <g.schelter@posteo.de>
2019-03-30 21:00:42 +0100

    Added counter

--- extraction.py
+++ extraction.py
@@ -1,5 +1,5 @@
 import itertools
-
+from collections import Counter
 import requests
 import json
 from rake_nltk import Rake
@@ -10,7 +10,27 @@ from bs4 import BeautifulSoup
 API_KEY = "ada358ce40ea4d53a3188656f4b0e5ec"
 
 
-def extract_text(html):
+def html2text(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.decompose()  # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    return text
+
+
+def extract_main_text(html):
     doc = Document(html)
     html_string = doc.summary(True)
 
@@ -37,17 +57,26 @@ def get_website_data(link: str):
     html = response.text
 
     data = extract_metadata(html)
-    data['text'] = extract_text(html)
+    data['text'] = html2text(html)
+    data['main_text'] = extract_main_text(html)
+    data['counter'] = count_words(data['text'])
+    data['phrases'] = extract_phrases(data['text'])
 
     return data
 
 
 def extract_phrases(text: str):
-    r = Rake(min_length=1, max_length=2)  # Uses stopwords for english from NLTK, and all puntuation characters.
+    r = Rake(min_length=1, max_length=4)  # Uses stopwords for english from NLTK, and all puntuation characters.
     r.extract_keywords_from_text(text)
 
     phrases = r.get_ranked_phrases()
     return phrases
+
+
+def count_words(text: str):
+    c = Counter(text.split())
+
+    return c.most_common(20)
 
 
 def get_data(term):
@@ -84,11 +113,14 @@ def get_podcasts_for_phrases(phrases):
 
 def combined(website_url: str):
     data = get_website_data(website_url)
-    website_text = data['text']
-    website_phrases = extract_phrases(website_text)
-    print(website_phrases)
 
-    podcasts = get_podcasts_for_phrases(website_phrases)
+    print('Counted words: {}', data['counter'])
+    print('Phrases: {}', data['phrases'])
+    print('Keywords: {}', data['keywords'])
+    print('Title: {}', data['title'])
+    print('Description: {}', data['description'])
+
+    podcasts = get_podcasts_for_phrases(data['phrases'])
 
     for podcast in podcasts:
         print(podcast['title_original'])
